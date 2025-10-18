b620fee6ed7a2265621b07fa3cc8c68b42a50d51
riccio8 <riccioadami@gmail.com>
2025-10-18 14:50:07 +0200

    Update test_denoise.rs

--- median/tests/test_denoise.rs
+++ median/tests/test_denoise.rs
@@ -8,11 +8,11 @@ fn test_denoise_rgb8() {
     let input_path = "tests/noise.jpg";
     let output_path = "tests/output_test.png";
 
-    assert!(Path::new(input_path).exists(), "File di test non trovato: {}", input_path);
+    assert!(Path::new(input_path).exists(), "Test file not found {}", input_path);
 
     let result = denoise(input_path, 3, ColorSpace::Rgb8);
     
-    assert!(result.is_ok(), "La funzione denoise ha fallito: {:?}", result.err());
+    assert!(result.is_ok(), "Denoise function failed {:?}", result.err());
     result.unwrap().save(output_path).unwrap();
-    assert!(Path::new(output_path).exists(), "File di output non creato");
+    assert!(Path::new(output_path).exists(), "Output file not created");
 }
