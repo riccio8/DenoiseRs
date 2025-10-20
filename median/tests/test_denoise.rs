7209f485d953b9d8957c16e88fac12bf387355b3
riccio8 <riccioadami@gmail.com>
2025-10-20 19:10:42 +0200

    Add test for denoising RGB16 images
    Introduced a new test function `test_denoise_rgb16` to verify denoising functionality for RGB16 color space. Updated test images to support the new test.

--- median/tests/test_denoise.rs
+++ median/tests/test_denoise.rs
@@ -4,7 +4,6 @@ use std::path::Path;
 
 #[test]
 fn test_denoise_rgb8() {
-
     let input_path = "tests/noise.jpg";
     let output_path = "tests/output_test.png";
 
@@ -16,3 +15,18 @@ fn test_denoise_rgb8() {
     result.unwrap().save(output_path).unwrap();
     assert!(Path::new(output_path).exists(), "Output file not created");
 }
+
+//#[test]
+fn test_denoise_rgb16() {
+    let input_path = "tests/noise.jpg";
+    let output_path = "tests/output1_test.png";
+
+    assert!(Path::new(input_path).exists(), "Test file not found {}", input_path);
+
+    let result = denoise(input_path, 3, ColorSpace::Rgb16);
+    
+    assert!(result.is_ok(), "Denoise function failed {:?}", result.err());
+    result.unwrap().save(output_path).unwrap();
+    assert!(Path::new(output_path).exists(), "Output file not created");
+}
+
